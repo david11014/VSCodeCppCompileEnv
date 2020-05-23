2e70251a0a8f1142118910ebcfea8fa1f71a3a21
david11014 <david11014@gmail.com>
2020-05-23 13:57:29 +0800

    create project

---
+++ TestProject/TestProject/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+	std::cout << "Hello, World\n";
+	std::vector<int> array;
+
+	array.emplace_back( 10 );
+	array.emplace_back( 15 );
+	array.emplace_back( 16 );
+
+	for( auto& i : array ) {
+		std::cout << i << std::endl;
+		/*int *i = (int*)0x1234567879;
+		*i = 10;*/
+	}
+
+	return 0;
+}
